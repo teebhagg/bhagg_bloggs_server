8882179e3593d52fdfa2e6469ac0c087c4783903
Joshua Ansah <Khalijonez777@gmsil.com>
2022-11-28 05:58:50 +0000

    JWT

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const router = require('./routers/routers')
 require('dotenv').config();
+const cors = require('cors');
 
 const port = process.env.PORT || 4000 ;
 
 // Middleware
 const app = express();
+app.use(cors());
 app.use(express.json());
 // app.use(cookieParser())
 const db_url = process.env.MONGO_URL
